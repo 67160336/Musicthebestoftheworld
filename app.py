2e22825a3cc8261f59cc99fbe6d97b4931739dde
67160336 <67160336@go.buu.ac.th>
2026-03-22 17:40:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ if model is not None and le is not None:
         if not submit:
             # ก่อนกดปุ่ม ให้แสดงรูปภาพต้อนรับ
             st.info("👈 ปรับจังหวะดนตรีด้านซ้าย แล้วกดปุ่มเพื่อดูผลลัพธ์!")
-            st.image("My_pic.jpg", caption="แอปแนะนำเพลงของฉัน 🎧", width=200)
+            st.image("My_pic.jpg", caption="แอปแนะนำเพลงของฉัน 🎧", width=300)
             
         else:
             # เมื่อกดปุ่ม วิเคราะห์และทายผล
